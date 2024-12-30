a6e77161eb133fbd9f7450dd2dee92f6f24395ff
Rodrigo Bezerra <rodbn87@gmail.com>
2024-12-30 20:29:13 +0000

    Ajustes na exibição de teams e players

--- champions_stats/views.py
+++ champions_stats/views.py
@@ -6,31 +6,33 @@ from champions_stats.models import Players, Teams
 
 
 def home(request):
-    # Buscando os times e jogadores
-    teams = Teams.objects.all()  # Obtendo todos os times
-    players = Players.objects.all()  # Obtendo todos os jogadores
+    # Todos os teams e players
+    all_teams = Teams.objects.all()
+    all_players = Players.objects.all()
 
-    # Passando os dados para o template
+    # Dados para o template
     return render(
         request,
         'champions_stats/home.html',
-        {'teams': teams, 'players': players}
+        {'teams': all_teams, 'players': all_players}
     )
 
 
 def players(request):
-    # Filtro de clube
+    # Filtro de teams
     club_filter = request.GET.get('club', None)
 
-    players = Players.objects.all()
+    all_players = Players.objects.all().order_by('id_team__team')
     if club_filter and club_filter != 'all':
-        players = players.filter(id_team__team__iexact=club_filter)
+        filtered_players = all_players.filter(id_team__team__iexact=club_filter)
+    else:
+        filtered_players = all_players
 
-    paginator = Paginator(players, 10)
+    paginator = Paginator(filtered_players, 10)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
 
-    # Lista de clubes para o filtro
+    # Lista de teams para o filtro
     clubs = Players.objects.values_list(
         'id_team__team', flat=True).distinct().order_by('id_team__team')
 
@@ -43,20 +45,22 @@ def players(request):
 def teams(request):
     country_filter = request.GET.get('country', None)
 
-    teams = Teams.objects.all()
+    all_teams = Teams.objects.all().order_by('country')
     if country_filter and country_filter != 'all':
-        teams = teams.filter(country__iexact=country_filter)
+        filtered_teams = all_teams.filter(country__iexact=country_filter)
+    else:
+        filtered_teams = all_teams
 
-    paginator = Paginator(teams, 10)
+    paginator = Paginator(filtered_teams, 10)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
 
-    # Pegue uma lista de países únicos
-    countries = Teams.objects.values_list(
+    # Lista de países únicos
+    countries_list = Teams.objects.values_list(
         'country',
         flat=True).distinct().order_by('country')
 
     return render(request, 'champions_stats/teams.html', {
         'page_obj': page_obj,
-        'countries': countries,
+        'countries': countries_list,
     })
